a08973eb1942f625bc0bb4f22219efd39d7644af
sweeyang <sweeyangsing@gmail.com>
2018-12-18 06:50:41 +0800

    commit

--- part1.py
+++ part1.py
@@ -1,3 +1,23 @@
-for x in range(3):
-    print('Hello World!')
-    print('Hello World!')
+import datetime as dt
+import matplotlib.pyplot as plt
+from matplotlib import style
+import pandas as pd
+import pandas_datareader.data as web
+
+style.use('ggplot')
+
+start = dt.datetime(2000,1,1)
+end = dt.datetime(2016,12,31)
+
+df = web.DataReader('CNP', 'robinhood', start, end)
+df.reset_index(inplace=True)
+df.drop(['symbol', 'interpolated', 'session'], axis=1, inplace=True)
+df.rename(index=str, columns={'close_price': 'Close', 
+                             'high_price': 'High',
+                             'low_price': 'Low',
+                             'open_price': 'Open',
+                             'volume': 'Volume',
+                             'begins_at': 'Date'}, inplace=True)
+df.set_index('Date', inplace=True)
+
+print(df.head())
